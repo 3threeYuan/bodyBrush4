5ce18efc031938dcdf871e88993986d08b700e1a
ttmmd55 <chenshuting47@gmail.com>
2024-06-04 22:15:32 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 let video;
 let poseNet;
 
-// Right wrist
-let rightWristX = 0;
-let rightWristY = 0;
+// Positions for wrists and ankles
+let rightWristX = 0, rightWristY = 0;
+let leftWristX = 0, leftWristY = 0;
+let rightAnkleX = 0, rightAnkleY = 0;
+let leftAnkleX = 0, leftAnkleY = 0;
 
-// Paths for the right wrist
-let rightWristPath1;
-let rightWristPath2;
+// Paths for the wrists and ankles
+let rightWristPath1, rightWristPath2;
+let leftWristPath1, leftWristPath2;
+let rightAnklePath1, rightAnklePath2;
+let leftAnklePath1, leftAnklePath2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -26,6 +30,12 @@ function setup() {
   // Initialize paths
   rightWristPath1 = new Path(255, 255, 255); // White path for right wrist
   rightWristPath2 = new Path(255, 255, 255); // White path for right wrist on the black background
+  leftWristPath1 = new Path(255, 0, 0); // Red path for left wrist
+  leftWristPath2 = new Path(255, 0, 0); // Red path for left wrist on the black background
+  rightAnklePath1 = new Path(0, 255, 0); // Green path for right ankle
+  rightAnklePath2 = new Path(0, 255, 0); // Green path for right ankle on the black background
+  leftAnklePath1 = new Path(0, 0, 255); // Blue path for left ankle
+  leftAnklePath2 = new Path(0, 0, 255); // Blue path for left ankle on the black background
   
   // Create a save button
   saveButton = createButton('Save Drawing');
@@ -37,9 +47,15 @@ function setup() {
 
 function gotPoses(poses) {
   if (poses.length > 0) {
-    // Update right wrist position
+    // Update wrist and ankle positions
     rightWristX = poses[0].pose.keypoints[10].position.x / 2;
     rightWristY = poses[0].pose.keypoints[10].position.y;
+    leftWristX = poses[0].pose.keypoints[9].position.x / 2;
+    leftWristY = poses[0].pose.keypoints[9].position.y;
+    rightAnkleX = poses[0].pose.keypoints[14].position.x / 2;
+    rightAnkleY = poses[0].pose.keypoints[14].position.y;
+    leftAnkleX = poses[0].pose.keypoints[13].position.x / 2;
+    leftAnkleY = poses[0].pose.keypoints[13].position.y;
   }
 }
 
@@ -54,9 +70,15 @@ function draw() {
   // Draw video on the left half of the canvas
   image(video, 0, 0, width / 2, height);
   
-  // Add current positions to paths for right wrist
+  // Add current positions to paths for wrists and ankles
   rightWristPath1.add(rightWristX - width / 4, rightWristY - height / 2);
   rightWristPath2.add(rightWristX + width / 3, rightWristY - height / 2);
+  leftWristPath1.add(leftWristX - width / 4, leftWristY - height / 2);
+  leftWristPath2.add(leftWristX + width / 3, leftWristY - height / 2);
+  rightAnklePath1.add(rightAnkleX - width / 4, rightAnkleY - height / 2);
+  rightAnklePath2.add(rightAnkleX + width / 3, rightAnkleY - height / 2);
+  leftAnklePath1.add(leftAnkleX - width / 4, leftAnkleY - height / 2);
+  leftAnklePath2.add(leftAnkleX + width / 3, leftAnkleY - height / 2);
   
   // Update and display each path
   rightWristPath1.update();
@@ -64,6 +86,24 @@ function draw() {
   
   rightWristPath2.update();
   rightWristPath2.display();
+  
+  leftWristPath1.update();
+  leftWristPath1.display();
+  
+  leftWristPath2.update();
+  leftWristPath2.display();
+  
+  rightAnklePath1.update();
+  rightAnklePath1.display();
+  
+  rightAnklePath2.update();
+  rightAnklePath2.display();
+  
+  leftAnklePath1.update();
+  leftAnklePath1.display();
+  
+  leftAnklePath2.update();
+  leftAnklePath2.display();
 }
 
 // Path class
